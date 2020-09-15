2d82122419868027474c23518b9bc82fb513e6ac
Joseph903 <69022946+Joseph903@users.noreply.github.com>
2020-09-15 19:27:14 +1000

    Update aquarium.js

--- aquarium.js
+++ aquarium.js
@@ -2,7 +2,7 @@ window.onload=function() {
     lengthObj = document.getElementById('txtLength');
     heightObj = document.getElementById('txtHeight');
     widthObj = document.getElementById('txtWidth');
-	costObj = document.getElementById('tdCost');
+    costObj = document.getElementById('tdCost');
     document.getElementById('btnReset').onclick = resetInputs;
     document.getElementById('btnCalcCost').onclick = calcCost;
 }
@@ -10,16 +10,20 @@ function resetInputs() {
     lengthObj.value = '';
     heightObj.value = '';
     widthObj.value = '';
-	costObj.innerHTML = '';	
+    costObj.innerHTML = ''; 
 }
 function calcCost() {
     var length = new Number(lengthObj.value);
     var height = new Number(heightObj.value);
-	var width = new Number(widthObj.value);
+    var width = new Number(widthObj.value);
     costObj.innerHTML = '';
     if(isNaN(length) || isNaN(height) || isNaN(width)) {
         alert('Invalid length or height or width');
         return;
     }
-costObj.innerHTML = 2*(length*height+width*height+length*width)*0.06
+	if (height >25) {
+costObj.innerHTML = "$" + (2*(length*height+width*height+length*width)*0.1+(height*4+length*2+width*2)*0.1+ 2*(length*height+width*height+length*width)*0.01)*1.1
+} else {
+	costObj.innerHTML = "$" + (2*(length*height+width*height+length*width)*0.06+(height*4+length*2+width*2)*0.1+ 2*(length*height+width*height+length*width)*0.01)*1.1
+}
 }
